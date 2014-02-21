6a1e2dd2accc148c860f60dce139ee8b6d8bebb0
adamjacobbecker <ad@mbecker.cc>
2014-02-21 10:29:32 -0500

    fix merge conflicts

--- rebuild_gh_pages.sh
+++ rebuild_gh_pages.sh
@@ -1,11 +1,7 @@
 #! /bin/sh
 
 git checkout gh-pages
-<<<<<<< HEAD
-git merge master
-=======
 git merge master -m "Merge master"
->>>>>>> master
 docco scrapers/states/ga/rfps.coffee
 git commit -am "Update docs"
 git push origin gh-pages
