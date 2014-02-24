3b56e2fd8fb0df5b89d1299767a955b1a1ae771c
adamjacobbecker <ad@mbecker.cc>
2014-02-23 21:04:11 -0500

    remove redundant 'states' folder

--- rebuild_gh_pages.sh
+++ rebuild_gh_pages.sh
@@ -2,7 +2,7 @@
 
 git checkout gh-pages
 git merge master -m "Merge master"
-docco scrapers/states/ga/rfps.coffee
+docco scrapers/ga/rfps.coffee
 git commit -am "Update docs"
 git push origin gh-pages
 git checkout master
