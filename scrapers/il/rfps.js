451e97447aef5ae47b30576b75ce1c03ce06638c
Mepuka Kessy <kessy002@umn.edu>
2014-03-03 22:47:47 -0600

    added type field, smarter item field loading and validation

--- scrapers/il/rfps.js
+++ scrapers/il/rfps.js
@@ -15,7 +15,17 @@ var FIELDS = {
     "Name": "contact_name",
     "Phone": "contact_phone",
     "E-Mail Address": "contact_email",
-    "description": "description"
+    "description": "description",
+    "Solicitation Type": "type"
+}
+
+// types of solicitations on Illinois Procurement Board and their associated
+// types as defined on the repo master list
+var RFP_TYPES = {
+    "Request for Proposal": "RFP",
+    "Invitation for Bid": "ITB",
+    "Request for Information": "RFI",
+    "Request for Quotes": "RFQ"
 }
 
 module.exports = function(opts, done) {
@@ -24,7 +34,15 @@ module.exports = function(opts, done) {
         async.mapLimit(links, 5, getSolicitationDetails, function(error, results) {
             if(error) { throw new Error(error); }
             console.log("Done scraping!")
-            done(results);
+
+            var validators = {
+                "type": function(type) {
+                    return _.indexOf(_.values(RFP_TYPES), type) > -1;
+                }
+            }
+            done(_.filter(results, function(item) {
+                return validateFields(item, validators);
+            }));
         });
     });
 }
@@ -43,7 +61,7 @@ function getSolicitationUrls(startUrl, cb) {
 // pull the desired fields from each solicitation
 function getSolicitationDetails(url, cb) {
 
-    // for each solicitation there 5 <table> elements with information as follows
+    // for each solicitation there 6 <table> elements with information as follows
     // 0 identification
     // 1 overview
     // 2 key information
@@ -55,16 +73,21 @@ function getSolicitationDetails(url, cb) {
 
     var rename = renamer(FIELDS);
     request.get(url, function(error, response, body) {
-        var $ = cheerio.load(body);
-        var funcs = [getInformation, getOverview, getInformation, getInformation, null, null];
+        var $ = cheerio.load(body),
+            funcs = [getInformation, getOverview, getInformation, getInformation, null, null],
+            loaders = {
+                "type": function(solicitation) {
+                    return RFP_TYPES[solicitation];
+                }
+            }
 
         var detailsByTable = $("br+table").map(function(i, table) {
             return _.isFunction(funcs[i]) ? rename(funcs[i](table)) : {};
         }).toArray().concat({"html_url": url});
 
-        cb(null, _.reduce(detailsByTable, function(memo, val, i) {
+        cb(null, loadFields(_.reduce(detailsByTable, function(memo, val, i) {
             return _.extend(memo, val);
-        }, detailsByTable[0]));
+        }, detailsByTable[0]), loaders));
     });
 }
 
@@ -94,4 +117,20 @@ function renamer(fields) {
         }, {});
     }
 }
-
+// apply a function to item fields as defined in loaders
+function loadFields(item, loaders) {
+    return _.reduce(loaders, function(memo, func, key) {
+        if(_.has(memo, key)) memo[key] = func(memo[key]);
+        return memo;
+    }, item)
+}
+// validate fields as defined in validators
+function validateFields(item, validators) {
+    return _.every(_.map(validators, function(func, key) {
+        if(_.has(item, key)) return func(item[key]);
+        return false;
+    }))
+}
+function existy(thing) {
+    return thing != null;
+}
